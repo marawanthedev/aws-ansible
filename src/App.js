adba2d3d07a2a4588949dfce02c5299b0bc7acbc
marawanthedev <61912691+marawanthedev@users.noreply.github.com>
2024-08-20 23:24:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello From EC2 Instance, well done hosting react app through nginx
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
